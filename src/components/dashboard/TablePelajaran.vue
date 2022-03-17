1bebe67efbb60910617fef0bba9694372a368457
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-03-17 12:43:25 +0700

    [Dashboard] - List Pelajaran

--- src/components/dashboard/TablePelajaran.vue
+++ src/components/dashboard/TablePelajaran.vue
@@ -1,147 +1,147 @@
 <template>
-    <div class="pt-5 mt-5">
-        <h2 class="pb-2">List Data Pelajaran</h2>
-            <v-data-table
-                :headers="headers"
-                :items="data"
-                :item-class="colorBackground"
-                class="elevation-1"
-                style="color:#FF5E3C"
+  <div class="pt-5 mt-5">
+    <h2 class="mb-8 black--text">List Pelajaran</h2>
+
+    <!-- list pelajaran -->
+    <v-row class="white rounded">
+      <!-- card  -->
+      <v-col cols="3">
+        <v-card
+          class="elevation-0 pt-10 rounded-xl"
+          color="#fdf9d3bf"
+          height="280"
+        >
+          <center>
+            <!-- ilustrasi -->
+            <v-img
+              src="../../assets/dashboard/Ilustrasi/pemula.svg"
+              width="80"
+              class="mb-2"
             >
-            <template v-slot:[`item.statusbelajar`]="{ item }">
-                <v-chip
-                    :color="getColor(item.statusbelajar)"
-                    dark
-                >
-                    {{ item.statusbelajar }}
-                </v-chip>
-            </template>
-            </v-data-table>
-    </div>
+            </v-img>
+            <!-- kategori card -->
+            <h3 style="font-size: 22px" class="">Pemula</h3>
+            <p>(Alfabet)</p>
+            <v-btn
+              color="#FF5E3C"
+              class="white--text caption text-capitalize elevation-0 px-8"
+              rounded
+              >Lihat detail</v-btn
+            >
+          </center>
+        </v-card>
+      </v-col>
+      <!-- end of card -->
+
+      <!-- card  -->
+      <v-col cols="3">
+        <v-card
+          class="elevation-0 pt-10 rounded-xl"
+          color="#fdf9d3bf"
+          height="280"
+        >
+          <center>
+            <!-- ilustrasi -->
+            <v-img
+              src="../../assets/dashboard/Ilustrasi/mahir.svg"
+              width="80"
+              class="mb-2"
+            >
+            </v-img>
+            <!-- kategori card -->
+            <h3 style="font-size: 22px" class="">Mahir</h3>
+            <p>(Suku kata)</p>
+            <v-btn
+              color="#FF5E3C"
+              class="white--text caption text-capitalize elevation-0 px-8"
+              rounded
+              >Lihat detail</v-btn
+            >
+          </center>
+        </v-card>
+      </v-col>
+      <!-- end of card -->
+
+      <!-- card  -->
+      <v-col cols="3">
+        <v-card
+          class="elevation-0 pt-10 rounded-xl"
+          color="#fdf9d3bf"
+          height="280"
+        >
+          <center>
+            <!-- ilustrasi -->
+            <v-img
+              src="../../assets/dashboard/Ilustrasi/cakap.svg"
+              width="80"
+              class="mb-2"
+            >
+            </v-img>
+            <!-- kategori card -->
+            <h3 style="font-size: 22px" class="">Cakap</h3>
+            <p>(Kata)</p>
+            <v-btn
+              color="#FF5E3C"
+              class="white--text caption text-capitalize elevation-0 px-8"
+              rounded
+              >Lihat detail</v-btn
+            >
+          </center>
+        </v-card>
+      </v-col>
+      <!-- end of card -->
+
+      <!-- card  -->
+      <v-col cols="3">
+        <v-card
+          class="elevation-0 pt-10 rounded-xl"
+          color="#fdf9d3bf"
+          height="280"
+        >
+          <center>
+            <!-- ilustrasi -->
+            <v-img
+              src="../../assets/dashboard/Ilustrasi/terampil.svg"
+              width="80"
+              class="mb-2"
+            >
+            </v-img>
+            <!-- kategori card -->
+            <h3 style="font-size: 22px" class="">Terampil</h3>
+            <p>(Kalimat)</p>
+            <v-btn
+              color="#FF5E3C"
+              class="white--text caption text-capitalize elevation-0 px-8"
+              rounded
+              >Lihat detail</v-btn
+            >
+          </center>
+        </v-card>
+      </v-col>
+      <!-- end of card -->
+    </v-row>
+    <!-- end of list pelajaran -->
+  </div>
 </template>
 
 <script>
-  export default {
-      name: 'TablePelajaran',
-    data () {
-      return {
-        headers: [
-          {
-            text: 'Kategori Learning',
-            align: 'start',
-            sortable: true,
-            value: 'kategori',
-          },
-          { text: 'Level', value: 'level' },
-          { text: 'Materi', value: 'materi' },
-          { text: 'Asset Gambar', value: 'assetGambar' },
-          { text: 'Asset Suara', value: 'assetSuara' },
-        ],
-        data: [
-          {
-            number: 1,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 2,
-            kategori: 'Suku Kata',
-            level: 5,
-            materi: 'Suku Kata 1',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 3,
-            kategori: 'Kalimat',
-            level: 8,
-            materi: 'Kalimat',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 4,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 5,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 6,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 7,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 8,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 9,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-          {
-            number: 10,
-            kategori: 'Alfabet',
-            level: 1,
-            materi: 'Alfabet',
-            assetGambar: 'image1.jpg',
-            assetSuara: 'suara1.mp4'
-          },
-        ],
-      }
-    },
-    methods: {
-      getColor (statusbelajar) {
-        if (statusbelajar === 'Aktif') return 'green'
-        else return 'red'
-      },
-        colorBackground (item) {
-        if (item.number % 2 === 1) return 'style-2'
-        else return 'style-1'
-    },
-    },
-  }
+export default {
+  name: "TablePelajaran",
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <style>
-  .style-1 {
-    background-color: white
-  }
-  .style-2 {
-    background-color: #FEFCE9
-  }
-  .data-table {
-    color: #FF5E3C;
-  }
+.style-1 {
+  background-color: white;
+}
+.style-2 {
+  background-color: #fefce9;
+}
+.data-table {
+  color: #ff5e3c;
+}
 </style>
